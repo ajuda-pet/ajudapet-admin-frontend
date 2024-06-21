6f882f91c356ac9e2d79965b31d30cf496efe08b
Lebackrobot <lebackman18@gmail.com>
2024-06-21 19:54:57 -0300

    chore: use navigate

--- src/components/molecules/sideBarHome/index.jsx
+++ src/components/molecules/sideBarHome/index.jsx
@@ -3,8 +3,10 @@ import './index.css'
 
 
 import { Col, Row } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom';
 
 const SideBarHome = ({ page }) => {
+    const navigate = useNavigate()
     const [showSidebar, setShowSidebar] = useState(true)
 
     const toggleSidebar = () => {
@@ -15,9 +17,9 @@ const SideBarHome = ({ page }) => {
         <>
             <Row className='align-items-center d-none d-sm-flex'>
                 <Col className='sidebar px-4'>
-                    <a className={`circle ${page === '/' ? 'home' : ''}`} href='/'><img src='./images/home.png' width='30' alt='Ícone de home' title='Home' /></a>
-                    <a className={`circle ${page === '/pets' ? 'home' : ''}`} href='/pets'><img src='./images/pet-icon-sidebar.png' width='30' alt='Ícone de animal de estimação' title='Adicionar Pet' /></a>
-                    <a className={`circle ${page === '/pontos' ? 'home' : ''}`} href='/pontos'><img src='./images/location-icon-sidebar.png' width='30' alt='Ícone de ponto de adoção' title='Adicionar Ponto de Adoção' /></a>
+                    <a className={`circle ${page === '/' ? 'home' : ''}`} onClick={() => navigate('/')}><img src='./images/home.png' width='30' alt='Ícone de home' title='Home' /></a>
+                    <a className={`circle ${page === '/pets' ? 'home' : ''}`} onClick={() => navigate('/pets')}><img src='./images/pet-icon-sidebar.png' width='30' alt='Ícone de animal de estimação' title='Adicionar Pet' /></a>
+                    <a className={`circle ${page === '/pontos' ? 'home' : ''}`} onClick={() => navigate('/pontos')}><img src='./images/location-icon-sidebar.png' width='30' alt='Ícone de ponto de adoção' title='Adicionar Ponto de Adoção' /></a>
                 </Col>
             </Row>
             
