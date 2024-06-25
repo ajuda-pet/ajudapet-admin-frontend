2ddb4eab29e21fbdbe8a99a9069dd211848eae55
Lebackrobot <lebackman18@gmail.com>
2024-06-25 13:09:10 -0300

    feat: "no content" component

---
+++ src/components/NoContent/NoContent.jsx
@@ -0,0 +1,17 @@
+import { Alert } from "react-bootstrap"
+
+const NoContent = ({ message }) => {
+    return (
+        <>
+            <center>
+                <img src='./images/no-content.png' width='350' className='mb-2'></img>
+            </center>
+            <center>
+                <strong style={{ fontSize: '17.7px' }} className='px-1'>🐾  &nbsp; {message}</strong>
+            </center>
+        </>
+    )
+
+}
+
+export default NoContent
